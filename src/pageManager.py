870957ae6ca189fdbab422a7731f041bbe3274e2
verawatisimatupang <88297371+verawatisimatupang@users.noreply.github.com>
2023-01-30 18:13:01 +0700

    Add files via upload

---
+++ src/pageManager.py
@@ -0,0 +1,24 @@
+from tkinter import Tk
+import Home, Vigenere
+
+class pageManager():
+    def __init__(self):
+        self.user = None
+        self.window = Tk()
+        self.window.geometry("1280x832")
+        self.window.configure(bg = "#F4F3F9")
+        self.window.title('Kriptografi dan Koding')
+        self.window.resizable(False, False)
+
+        self.page = Home.HomePage(master = self.window, pageManager = self)
+
+    def run(self):
+        self.page.startPage()
+    
+    def Home(self):
+        self.page = Home.HomePage(master = self.window, pageManager = self)
+        self.page.startPage()
+
+    def Vigenere(self):
+        self.page = Vigenere.VigenerePage(master = self.window, pageManager = self)
+        self.page.startPage()
